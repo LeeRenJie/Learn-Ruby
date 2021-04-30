8cce4900f72dced993a0e1b111a7b8850cb3cda0
leerenjie <work.renjie@gmail.com>
2021-04-30 20:40:42 +0800

    feat: add link to delete article

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -37,4 +37,11 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @article
+  end
+
 end
