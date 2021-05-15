aaff2d6b386d129b1cccb77779994c80e3d95886
leerenjie <work.renjie@gmail.com>
2021-05-15 15:14:48 +0800

    feat: add password_digest field in user table

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitve: false }, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitve: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
 
