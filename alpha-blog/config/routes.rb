7783bb03745f07528073231c55b9c95af93a0b23
leerenjie <work.renjie@gmail.com>
2021-05-15 19:13:02 +0800

    feat: sign up for for user to enter their username email and password

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
-
   get 'about', to: 'pages#about'
-
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
 
