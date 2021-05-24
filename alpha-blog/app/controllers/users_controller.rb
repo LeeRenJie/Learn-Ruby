9ca38991953b3de3476f6ba655cfb85ed22cd50d
leerenjie <work.renjie@gmail.com>
2021-05-24 20:58:25 +0800

    feat: create new users from backend

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -4,7 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
