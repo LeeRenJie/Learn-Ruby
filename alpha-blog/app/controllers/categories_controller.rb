4cf8eff7009beb48710fb94bf35377f8dba81506
leerenjie <work.renjie@gmail.com>
2021-06-10 20:39:33 +0800

    feat(categories): add views to create categories from browser

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -1,6 +1,16 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was succesfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -11,5 +21,10 @@ class CategoriesController < ApplicationController
 
   end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
 
