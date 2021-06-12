a600a1e0414bb6361108988ddd0ee6c52dbcd7ab
leerenjie <work.renjie@gmail.com>
2021-06-12 20:38:25 +0800

    feat(nav): update nav for admin to create category and all to view categories

--- alpha-blog/app/controllers/sessions_controller.rb
+++ alpha-blog/app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
+    flash[:alert] = "Logged out"
     redirect_to root_path
   end
 
