c8f419c1da5eb1bbe573a5b9475fc0ebc14ff451
leerenjie <work.renjie@gmail.com>
2021-07-09 20:51:43 +0800

    refactor(user model): remove belongs_to parent

--- test-app/app/models/user.rb
+++ test-app/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_one :article
   has_many :children, class_name: "User", foreign_key: "parent_id"
-  belongs_to :parent, class_name: "User", optional: true
   has_many :grandchildren, through: :children, source: :children
 end
