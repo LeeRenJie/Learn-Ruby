61ac422a98604e32a9b08d0d1ee2af2553703a9a
leerenjie <work.renjie@gmail.com>
2021-07-09 20:35:09 +0800

    feat(user model): user has many children and grandchildren

--- test-app/app/models/user.rb
+++ test-app/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_one :article
-  belongs_to :mother, class_name: "User", optional: true
-  belongs_to :father, class_name: "User", optional: true
-  has_one :grandfather, through: :father, source: :father
-  has_one :grandmother, through: :father, source: :mother
+  has_many :children, class_name: "User", foreign_key: "parent_id"
+  belongs_to :parent, class_name: "User", optional: true
+  has_many :grandchildren, through: :children, source: :children
 end
--- test-app/db/migrate/20210701100245_create_users.rb
+++ test-app/db/migrate/20210701100245_create_users.rb
@@ -2,8 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.references :mother
-      t.references :father
+      t.references :parent
     end
   end
 end
