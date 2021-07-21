4e7c767941b301a3dfa4015f811c4faad3ee00c8
leerenjie <work.renjie@gmail.com>
2021-07-21 18:39:54 +0800

    feat(meesage model): add message model

--- message-me/db/schema.rb
+++ message-me/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_100140) do
+ActiveRecord::Schema.define(version: 2021_07_21_103106) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
