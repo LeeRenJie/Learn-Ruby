99b1237370de17e79dff9c70a9028be5d210a74c
leerenjie <work.renjie@gmail.com>
2021-07-22 16:29:13 +0800

    refactor(user model and message migration): dependant destroy in user model,t.text for message body

--- message-me/db/schema.rb
+++ message-me/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_07_21_103106) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "body"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
