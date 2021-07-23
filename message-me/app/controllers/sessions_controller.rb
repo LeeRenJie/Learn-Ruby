c2b83f926bafd04376bf701055310cef3189be89
leerenjie <work.renjie@gmail.com>
2021-07-23 18:21:38 +0800

    refactor(sessions_controller): use ampersand dot for user&.authenticate

--- message-me/app/controllers/sessions_controller.rb
+++ message-me/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You are now logged in"
       redirect_to root_path
