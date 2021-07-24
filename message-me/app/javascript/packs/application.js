84d6c5210f4f2f250af607ad308af9d12c5f7cf4
leerenjie <work.renjie@gmail.com>
2021-07-24 15:27:49 +0800

    revert(application.js): revert back to use function()

--- message-me/app/javascript/packs/application.js
+++ message-me/app/javascript/packs/application.js
@@ -12,9 +12,9 @@ import '@doabit/semantic-ui-sass'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-document.addEventListener('turbolinks:load', () => {
+$(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
-  $('.message .close').on('click', () =>{
+  $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
 })
