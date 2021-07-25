683ec7aa41889b6e632798167d63fb79156f835a
leerenjie <work.renjie@gmail.com>
2021-07-25 20:18:24 +0800

    refactor(application.js): refactor scroll_bottom function

--- message-me/app/javascript/packs/application.js
+++ message-me/app/javascript/packs/application.js
@@ -13,16 +13,18 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-window.scroll_bottom = function() {
-  if ($('#messages').length > 0) {
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+window.scroll_bottom = function(id) {
+  const element = $(`#${id}`)
+  const scrollHeight = element[0].scrollHeight;
+  if (id.length > 0){
+    element.scrollTop(scrollHeight);
   }
-}
+};
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message').click(function() {
     $(this).transition('fade');
   });
-  scroll_bottom();
+  scroll_bottom('messages');
 });
