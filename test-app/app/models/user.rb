8423c119bf2c347797fb3b54776517025ad2dc27
leerenjie <work.renjie@gmail.com>
2021-07-29 22:14:38 +0800

    style(user.rb): group methods together

--- test-app/app/models/user.rb
+++ test-app/app/models/user.rb
@@ -1,21 +1,19 @@
 class User < ApplicationRecord
   has_one :articles
-
+  has_one :grandfather, through: :father, source: :father
+  has_one :grandmother, through: :father, source: :mother
   has_many :children,
   -> (user) { unscope(:where).where("father_id = :id OR mother_id = :id", id: user.id) },
    class_name: "User"
 
+  belongs_to :mother, class_name: "User", optional: true
+  belongs_to :father, class_name: "User", optional: true
+
   def grandchildren
     user_id = self.id
-
     User.where(
       "father_id IN (:ids) OR mother_id IN (:ids)",
       ids: User.where("father_id = :id OR mother_id = :id", id: user_id).pluck(:id)
     )
   end
-
-  belongs_to :mother, class_name: "User", optional: true
-  belongs_to :father, class_name: "User", optional: true
-  has_one :grandfather, through: :father, source: :father
-  has_one :grandmother, through: :father, source: :mother
 end
