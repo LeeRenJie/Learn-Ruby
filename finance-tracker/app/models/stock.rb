e06bccf22230555fed03e0f3fdd9ec869549b49d
leerenjie <work.renjie@gmail.com>
2021-08-04 21:34:57 +0800

    feat: add api keys in encrypted file and reference in method

--- finance-tracker/app/models/stock.rb
+++ finance-tracker/app/models/stock.rb
@@ -1,8 +1,8 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: 'publishable_token',
-      secret_token: 'secret_token',
+      publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+      secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
       endpoint: 'https://sandbox.iexapis.com/v1')
     client.price(ticker_symbol)
   end
