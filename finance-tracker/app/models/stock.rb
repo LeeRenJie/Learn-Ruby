14a3fcfeb0353b63e68eb3db7a77a288ca345cd9
leerenjie <work.renjie@gmail.com>
2021-08-08 15:30:44 +0800

    style: closing bracket to next line

--- finance-tracker/app/models/stock.rb
+++ finance-tracker/app/models/stock.rb
@@ -3,7 +3,8 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(
       publishable_token: 'publishable_token',
       secret_token: 'secret_token',
-      endpoint: 'https://sandbox.iexapis.com/v1')
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
     client.price(ticker_symbol)
   end
 end
