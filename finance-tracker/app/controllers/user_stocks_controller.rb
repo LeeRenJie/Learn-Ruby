63c195f18e1d7e7d27a91bf22f1c6d21401353b8
leerenjie <work.renjie@gmail.com>
2021-08-11 17:06:00 +0800

    feat: able to remove tracking of stocks from frontend

--- finance-tracker/app/controllers/user_stocks_controller.rb
+++ finance-tracker/app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to portfolio_index_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "Stock #{stock.ticker} was successfully removed from your portfolio"
+    redirect_to portfolio_index_path
+  end
 end
