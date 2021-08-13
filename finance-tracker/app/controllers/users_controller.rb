979490c289a1d144fa53fc295851b85d71119585
leerenjie <work.renjie@gmail.com>
2021-08-13 21:53:57 +0800

    feat: add search form and methods to search for users from backend

--- finance-tracker/app/controllers/users_controller.rb
+++ finance-tracker/app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
+  def friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
 
 end
