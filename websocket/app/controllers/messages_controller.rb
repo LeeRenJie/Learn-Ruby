6f46bbc974b1c496c085b626aa654f53f917f3ad
leerenjie <work.renjie@gmail.com>
2021-09-11 20:50:30 +0800

    feat: recreate app

--- websocket/app/controllers/messages_controller.rb
+++
@@ -1,9 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-  end
-
-  def create
-    Message.create(content: params[:message])
-    ActionCable.server.broadcast('messages', { messages: Message.all })
-  end
-end
