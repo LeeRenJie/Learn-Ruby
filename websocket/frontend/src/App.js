2e0641b0a12626236790a7d60c421b3b38e50177
leerenjie <work.renjie@gmail.com>
2021-09-23 23:34:28 +0800

    feat: init project for react frontend and rails backend

--- websocket/frontend/src/App.js
+++ websocket/frontend/src/App.js
@@ -1,15 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import TodoList from './todolist/TodoList'
-import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant="h1">Todo Stream</Typography>
-        <hr/>
-        <TodoList/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
--- websocket/frontend/src/todolist/TodoItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function TodoItem(props) {
-  return (
-    <li>{props.item.task}</li>
-  )
-}
