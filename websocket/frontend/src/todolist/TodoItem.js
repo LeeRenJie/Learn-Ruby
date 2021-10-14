df9aa137b0cd22f722e756f3af19d8e532a67753
leerenjie <work.renjie@gmail.com>
2021-10-14 20:52:13 +0800

    chore: merge master into websocket-cleanup

--- websocket/frontend/src/todolist/TodoItem.js
+++ websocket/frontend/src/todolist/TodoItem.js
@@ -4,13 +4,30 @@ import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = ({
   root: {
     padding: "2em",
     margin: "1em",
     height: "auto"
-  }
+  },
+  divider: {
+    margin: "1em",
+    marginLeft:"37.5%",
+    width: "25%"
+  },
+  paper: {
+    margin: "1em",
+    padding: "2em",
+    textAlign: "justify"
+  },
+  heading: {
+    textAlign: "center"
+  },
+  body: {
+    padding: "1em"
+  },
 });
 
 class TodoItem extends Component {
@@ -27,12 +44,18 @@ class TodoItem extends Component {
     const { classes } = this.props;
     return (
       <Grid container spacing={0}>
-        <Grid item xs={9}>
-          <Paper elevation={3} className={classes.root}>
-            {this.props.item.task}
-            <hr/>
-            {this.props.item.body}
-            <hr/>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={10}>
+          <Paper elevation={10} className={classes.paper}>
+            <Typography variant="h4" className={classes.heading}>
+              {this.props.item.task}
+            </Typography>
+            <hr className={classes.divider}/>
+            <div className={classes.body}>
+              <Typography variant="body1">
+                {this.props.item.body}
+              </Typography>
+            </div>
             <Button
               variant="contained"
               color="secondary"
@@ -48,5 +71,4 @@ class TodoItem extends Component {
     );
   };
 };
-
 export default withStyles(useStyles)(TodoItem);
