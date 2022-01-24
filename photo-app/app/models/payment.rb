53b2f5030c25781b13ec9e1e706515e1d594b185
leerenjie <work.renjie@gmail.com>
2022-01-24 20:14:37 +0800

    refactor: indent argument and change data type from string to text

--- photo-app/app/models/payment.rb
+++ photo-app/app/models/payment.rb
@@ -1,6 +1,7 @@
 class Payment < ApplicationRecord
   attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
   belongs_to :user
+
   def self.month_options
     Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
   end
@@ -12,8 +13,8 @@ class Payment < ApplicationRecord
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
     Stripe::Charge.create customer: customer.id,
-    amount: 1000,
-    description: 'Premium',
-    currency: 'usd'
+                          amount: 1000,
+                          description: 'Premium',
+                          currency: 'usd'
   end
 end
--- photo-app/db/migrate/20220122130633_create_payments.rb
+++ photo-app/db/migrate/20220122130633_create_payments.rb
@@ -1,8 +1,8 @@
 class CreatePayments < ActiveRecord::Migration[6.1]
   def change
     create_table :payments do |t|
-      t.string :email
-      t.string :token
+      t.text :email
+      t.text :token
       t.integer :user_id
 
       t.timestamps
